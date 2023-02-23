ca778d369e028de8336aa7c6e70f8e4ec3fb5810
leonardopol <leonardo_pol@hotmail.com>
2023-02-23 13:35:35 -0300

    inicio tarea 1 clase 7
    se cambia el js principal para probar las validaciones en el archivo tarea-clase-7.js

--- tarea/tarea-clase-7.js
+++ tarea/tarea-clase-7.js
@@ -5,3 +5,13 @@
 * Adicional: Escribir pruebas para las funciones de tareas anteriores.
 *
 * */
+
+const $form = document.querySelector('#carta-a-santa');
+const ciudad = $form.ciudad.value;
+
+function validarCiudad(ciudad){
+    if(ciudad === '') {
+        'El campo no puede estar vacio';
+    }
+}
+
