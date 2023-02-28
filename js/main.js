b4115e68374eabfe4f5c81cc449cdda356d99eef
leonardopol <leonardo_pol@hotmail.com>
2023-02-27 22:56:39 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -47,18 +47,21 @@ function validarDescripcionRegalo(descripcionRegalo){
  function validarFormulario(event){
     const $form = document.querySelector('#carta-a-santa');
 
+    // este nombre, ciudad y descripcionRegalo que recojen los valores de los campos a validar no tienen nada que ver
+    // con nombre, ciudad, 'descripcion-regalo' que estan como keys en el objeto errores.
     const nombre = $form.nombre.value;
     const ciudad = $form.ciudad.value;
     const descripcionRegalo = $form['descripcion-regalo'].value;
-
+    
     const errorNombre = validarNombre(nombre);
     const errorCiudad = validarCiudad(ciudad);
     const errorDescripcionRegalo = validarDescripcionRegalo(descripcionRegalo);
 
+    //toma los nombres del campo name de los inputs del html
     const errores = {
         nombre: errorNombre,  
         ciudad: errorCiudad,
-        descripcionRegalo: errorDescripcionRegalo
+        'descripcion-regalo': errorDescripcionRegalo //tiene que coincidir con el nombre que esta en el parametro name del html
     };
 
     console.log(errores);
@@ -67,8 +70,29 @@ function validarDescripcionRegalo(descripcionRegalo){
 
     event.preventDefault();
  }
-
+//Manera dinamica automatico
  function manejarErrores(errores){
+
+    //tomamos las llaves del objeto
+    const keys = Object.keys(errores);
+    //y las recorremos con el forEach
+    keys.forEach(function(key){
+        //toma el error en la posicion 0 y asi susecivamente
+        const error = errores[key];
+        //si hay error (true) pone el marco rojo del css cambiando el className si es falso lo pone en blanco ''
+        if(error){
+            $form[key].className = "error";
+        } else {
+            $form[key].className = "";
+        }
+    });
+
+    const $form = document.querySelector('#carta-a-santa');
+    $form.onsubmit = validarFormulario;
+
+    /*
+    Manera estatica de hacerlo uno por uno
+
     errorNombre = errores.nombre; //nombre
     errorCiudad = errores.ciudad; //ciudad
     errorDescripcionRegalo = errores.descripcionRegalo ; //descripcionRegalo
@@ -89,8 +113,8 @@ function validarDescripcionRegalo(descripcionRegalo){
         $form['descripcion-regalo'].className = 'error';
     } else {
         $form['descripcion-regalo'].className = '';
-    }
+    }*/
  }
 
- const $form = document.querySelector('#carta-a-santa');
- $form.onsubmit = validarFormulario;
+ //const $form = document.querySelector('#carta-a-santa');
+ //$form.onsubmit = validarFormulario;
